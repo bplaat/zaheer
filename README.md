afc5d7eeea03b1ebca787f0a0d84761fa30229f9
Bastiaan van der Plaat <bastiaan.v.d.plaat@gmail.com>
2025-08-17 13:23:24 +0200

    FPGA: Load uart bytes from external file

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 A work in progress complete computer desktop GUI platform running on cheap FPGA's
 
+## Getting Started
+
+- Install [oss-cad-suite](https://github.com/YosysHQ/oss-cad-suite-build) and add to path
+- Run `cd fpga; make load` and to flash `make flash`
+
 ## License
 
 Copyright © 2020-2025 [Bastiaan van der Plaat](https://github.com/bplaat)
--- fpga/top.v
+++ fpga/top.v
@@ -38,43 +38,35 @@ uart_tx #(
 
 // ROM
 reg [7:0] rom [0:1023];
-
 initial begin
-    rom[0] = "B";
-    rom[1] = "a";
-    rom[2] = "s";
-    rom[3] = "t";
-    rom[4] = "i";
-    rom[5] = "a";
-    rom[6] = "a";
-    rom[7] = "n";
-    rom[8] = " ";
-    rom[9] = 8'h00;
+    $readmemh("boot.mem", rom);
 end
 
 // LED counter
-localparam WAIT_TIME = CLK_FREQ / 16;
+localparam WAIT_TIME = CLK_FREQ / 100;
 
 reg [23:0] clock_counter = 0;
 reg [5:0] led_counter = 0;
 reg [31:0] instruction_pointer = 0;
 
 always @(posedge clk) begin
-    clock_counter <= clock_counter + 1;
-    if (clock_counter == WAIT_TIME) begin
-        clock_counter <= 0;
-        led_counter <= led_counter + 1;
+    if (~rst) begin
+        clock_counter <= clock_counter + 1;
+        if (clock_counter == WAIT_TIME) begin
+            clock_counter <= 0;
+            led_counter <= led_counter + 1;
 
-        if (rom[instruction_pointer] == 8'h00) begin
-            instruction_pointer <= 0;
+            if (rom[instruction_pointer] == 8'h00) begin
+                instruction_pointer <= 0;
+            end else begin
+                uart_data <= rom[instruction_pointer];
+                uart_start <= 1;
+
+                instruction_pointer <= instruction_pointer + 1;
+            end
         end else begin
-            uart_data <= rom[instruction_pointer];
-            uart_start <= 1;
-
-            instruction_pointer <= instruction_pointer + 1;
+            uart_start <= 0;
         end
-    end else begin
-        uart_start <= 0;
     end
 end
 
